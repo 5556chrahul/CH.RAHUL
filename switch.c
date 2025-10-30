4c11af312e732e62450cea41474066bcf12d159e
chithapally Rahul <5556chrahul@gmail.com>
2025-10-30 19:38:40 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,31 +1,19 @@
-#include<stdio.h>
-main (void)
-{
-	int a,b;
-	int add,sub,mul,mod;
-	float div;
-	char op;
-	printf("enter the opration:");
-	scanf("%c",&op);
-	printf("enter any two values to perform the operation %c/n :",op);
-	scanf ("%d",&a,&b);
-	switch(op)
-	{
-		case '+': add = a+b;
-		          printf("add %d",add);
-		          break;
-		case '-':  sub = a-b;
-		          printf("sub %d",sub);
-				  break;
-		case '*':  mul = a*b;
-		          printf("mul %d",mul);
-				  break;
-		case '/':  div = (float)a/b;
-		          printf("division:%f",div);
-				  break;
-		case '%':  mod = a%b;
-		          printf("remainder:%d",mod);
-				  break;
-		default : printf("sorry invalid option :(");		  		  	  	          
-	}
+#include <stdio.h>
+int main() {
+    int a, b;
+    char op;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    switch(op) {
+        case '+': printf("Sum = %d\n", a + b); break;
+        case '-': printf("Difference = %d\n", a - b); break;
+        case '*': printf("Product = %d\n", a * b); break;
+        case '/': printf("Quotient = %d\n", a / b); break;
+        default: printf("Invalid operator\n");
+    }
+    return 0;
 }
+
